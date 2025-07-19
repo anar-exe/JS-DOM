3801326eec870418b3fef66b2f6dcb625a89c1f0
Anar <anarq2006q@proton.me>
2025-07-19 12:44:57 +0000

    ready

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 "use strict";
 $(document).ready(function () {
 
+    $('.btn').click(function () {
+        let val = $('input').val();
+
+        if ($.isNumeric(val)) {
+            let num = parseInt(val);
+            for (let i = 0; i < num; i++) {
+                $('.output').append(`<div class="minibox d-flex justify-content-center"><h1>${num}</h1></div>`);
+            }
+        } else {
+            $('.output').append(`<div class="minibox d-flex justify-content-center"><h1>${val}</h1></div>`);
+        }
+
+        $('input').val('');
+    });
 });
